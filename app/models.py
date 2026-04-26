9feae771629acfc7ecdb0db1d71c8d8cbd784215
ahmadhabibshovo <ahmadhabibshovo@gmail.com>
2026-04-26 22:35:55 +0600

    feat: add Course and User models and their corresponding Pydantic schemas

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, TIMESTAMP
+from sqlalchemy.sql import func
 from .database import Base
 
 class Course(Base):
@@ -9,3 +10,13 @@ class Course(Base):
     instructor = Column(String, nullable=False)
     duration = Column(Float, nullable=False)
     website = Column(String, nullable=False)
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now())
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
+    
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, HttpUrl, ConfigDict
 from typing import Optional
+from datetime import datetime
 
 class CourseBase(BaseModel):
     name: str
@@ -20,3 +21,16 @@ class Course(CourseBase):
     id: int
 
     model_config = ConfigDict(from_attributes=True)
+
+class UserBase(BaseModel):
+    email: str
+
+class UserCreate(UserBase):
+    password: str
+
+class UserOut(UserBase):
+    id: int
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
